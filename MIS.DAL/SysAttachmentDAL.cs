b81ff5e0c905adfe152d99727ea54169f475a144
zhangyang3@e-fpg.com.cn <zhangyang3@e-fpg.com.cn>
2020-03-04 17:32:19 +0800

    附件

---
+++ MIS.DAL/SysAttachmentDAL.cs
@@ -0,0 +1,42 @@
+﻿using MIS.EFDataSource;
+using MIS.Model.Result;
+using MIS.Model.Sys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIS.DAL
+{
+   public class SysAttachmentDAL
+    {
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="inputForm"></param>
+        /// <returns></returns>
+        public RequestResult Add(SysAttachmentInputForm inputForm)
+        {
+            RequestResult result = new RequestResult();
+
+            try
+            {
+                MISEntities db = new MISEntities();
+
+
+
+
+
+
+                db.SaveChanges();
+                result.Success();
+            }
+            catch (Exception ex)
+            {
+                result.ReturnFailedMessage(ex.Message);
+            }
+            return result;
+        }
+    }
+}
---
+++ MIS.Model/Sys/SysAttachmentInputForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIS.Model.Sys
+{
+   public class SysAttachmentInputForm
+    {
+        /// <summary>
+        /// 文件大小
+        /// </summary>
+       public int FileSize { get; set; }
+
+        /// <summary>
+        /// 文件类型
+        /// </summary>
+        public string FileType { get; set; }
+
+        /// <summary>
+        /// 保存文件的路径
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// 文件名称
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 文件扩展名
+        /// </summary>
+        public string FileExtension { get; set; }
+    }
+}
