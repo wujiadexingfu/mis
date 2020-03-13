f91af78802a703c12e54f9c618ee9dcd66053bd8
zhangyang3@e-fpg.com.cn <zhangyang3@e-fpg.com.cn>
2020-03-13 12:10:35 +0800

    新增流程设计

---
+++ MIS.UI/Areas/WorkFlow/Controllers/WorkFlowInstanceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MIS.UI.Areas.WorkFlow.Controllers
+{
+    public class WorkFlowInstanceController : Controller
+    {
+       
+        public ActionResult Index(string tableName,string workFlowInstanceUniqueId)
+        {
+            ViewBag.TableName = tableName;
+            ViewBag.workFlowInstanceUniqueId = workFlowInstanceUniqueId;
+            return View();
+        }
+    }
+}
